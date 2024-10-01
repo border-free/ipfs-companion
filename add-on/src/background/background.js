b0ed73ee245a71dee618a1e885a711922aec05a2
blackbird <blackbirdbop@gmail.com>
2024-10-01 18:01:27 +0900

    BF disable onInstalled event

--- add-on/src/background/background.js
+++ add-on/src/background/background.js
@@ -3,11 +3,11 @@
 
 import browser from 'webextension-polyfill'
 import createIpfsCompanion from '../lib/ipfs-companion.js'
-import { onInstalled } from '../lib/on-installed.js'
+// import { onInstalled } from '../lib/on-installed.js'
 import { getUninstallURL } from '../lib/on-uninstalled.js'
 
 // register lifecycle hooks early, otherwise we miss first install event
-browser.runtime.onInstalled.addListener(onInstalled)
+// browser.runtime.onInstalled.addListener(onInstalled)
 browser.runtime.setUninstallURL(getUninstallURL(browser))
 
 const init = async () => {
